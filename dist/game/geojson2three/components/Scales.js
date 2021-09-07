669076ebc174a7b4531bbe1e9f4b59a7bc734f19
Orzo Cogorzo <orzocogorzo@hotmail.com>
2021-09-07 23:55:02 +0200

    Refactor release

---
+++ dist/game/geojson2three/components/Scales.js
@@ -0,0 +1,50 @@
+function Scale(range, domain) {
+  var _range = Math.abs(range[0] - range[1]);
+
+  var _domain = Math.abs(domain[0] - domain[1]);
+
+  var fn = function (value) {
+    return (value - domain[0]) / _domain * _range - _range / 2;
+  };
+
+  fn.range = function () {
+    return JSON.parse(JSON.stringify(range));
+  };
+
+  fn.domain = function () {
+    return JSON.parse(JSON.stringify(domain));
+  };
+
+  return fn;
+}
+
+function AbsoluteScale(domain) {
+  const _domain = domain[1] - domain[0];
+
+  return function (val) {
+    return val / 180 * _domain;
+  };
+}
+
+function RelativeScale(range, domain) {
+  const _domain = domain[1] - domain[0];
+
+  const _range = range[1] - range[0];
+
+  const scale = function (val) {
+    return (val - range[0]) / _range * _domain;
+  };
+
+  scale._domain = _domain;
+  scale._range = _range;
+  return scale;
+}
+
+function ProjectedScale(projection, range, domain) {
+  const transformation = proj4(projection, "EPSG:4326");
+  return function (coord) {
+    return pixelScale(transformation.forward(coord));
+  };
+}
+
+export { AbsoluteScale, RelativeScale, ProjectedScale, Scale };
---
+++ dist/game/scene/Lights.js
@@ -0,0 +1,13 @@
+class Lights extends THREE.Group {
+  constructor() {
+    super();
+    const ambientLight = new THREE.AmbientLight(0x999999, 0.8);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
+    directionalLight.position.set(1000, 0, 2000);
+    this.add(ambientLight);
+    this.add(directionalLight);
+  }
+
+}
+
+export default Lights;
